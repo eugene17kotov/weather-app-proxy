eb6bfd4a362d888013d60e241ecb9b96b7546957
Yevhenii Kobets <eu.kobets@gmail.com>
2023-06-02 05:32:37 +0300

    add error handler

--- proxyController.js
+++ proxyController.js
@@ -1,14 +1,23 @@
 const axios = require('axios');
-const dotenv = require('dotenv').config();
+require('dotenv').config();
+const customError = require('./error');
+
+const access_key = process.env.API_KEY;
 
 axios.defaults.baseURL = 'http://api.weatherstack.com';
 
 const proxyController = async (req, res) => {
     const { query = 'Dnipropetrovsk' } = req.query;
 
-    const params = { access_key: '3b85bff172787a1251e3f8d9a1d3275a', query };
+    const params = { access_key, query };
 
     const data = await axios.get('/current', { params });
+
+    console.log(data.data.success);
+
+    if (data.data.success === false) {
+        throw customError({ status: 400, message: 'Invalid city name' });
+    }
 
     const {
         data: {
